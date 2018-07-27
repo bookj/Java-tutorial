6c87c8f8bc15530f89da55b17d93e0bb9df11b5c
bookj <5710110137@email.psu.ac.th>
2018-07-27 13:47:44 +0700

    6 - สอน Java: การเขียนโปรแกรมเบื้องต้น ตอนที่ 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world ไทย สวัสดี");
+    public static void main(String[] args) { // method
+        System.out.println(3 * 5);
+        System.out.println("area = " + 3 * 5);
+
+        int width;  // -2^31 to 2^31-1
+        int height;
+        int area;
+
+//        width = 3;
+//        height = 5;
+//        area = width * height;
+
+//        System.out.println("area = " + area);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter width: ");
+        width = in.nextInt();
+//        System.out.println("Print width = " + width);
+        System.out.print("Enter height: ");
+        height = in.nextInt();
+        area = width * height;
+        System.out.println("area (" + width + " x " + height + ") = " + area);
     }
 }
